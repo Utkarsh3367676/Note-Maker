93a349e85d8d48928a439734af5e2783d6c7f193
utkarsh parihar <utkarshparihar3367@gmail.com>
2023-07-30 14:07:14 +0530

    committed the working Module

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CheckIcon from "@mui/icons-material/Check";
+
+
+function Header() {
+  return (
+    <header>
+      <h1 >
+        <CheckIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
